d7154f220efe4e78c75f351a814b7da2337405c1
kimjisu <ddc02336@gmail.com>
2025-05-19 15:28:18 +0900

    faq_레이아웃

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -3,5 +3,30 @@ import Navigation from "@/components/Navigation.vue";
 </script>
 <template>
   <Navigation />
-  FAQ 지수
+  <div class="wrap">
+    <ul class="taps_wrap">
+      <li>
+        <label for="total">전체</label>
+        <input style="position:absolute "  type="radio" id="total" />
+      </li>
+      <li>
+        <label for="reservation">예약관련</label>
+        <input type="radio" id="reservation" />
+      </li>
+      <li>
+        <label for="service">서비스관련</label>
+        <input type="radio" id="service" />
+      </li>
+      <li>
+        <label for="pay">결제관련</label>
+        <input type="radio" id="pay" />
+      </li>
+      <li>
+        <label for="cr">취소/환불</label>
+        <input type="radio" id="cr" />
+      </li>
+    </ul>
+    <div class="faq_list_wrap"></div>
+    <div class="info_sec"></div>
+  </div>
 </template>
